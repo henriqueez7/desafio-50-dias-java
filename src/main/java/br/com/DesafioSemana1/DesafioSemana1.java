7f08fdf86e67a9d6a4fb66bcaa2f91c7b36a2692
Gerenciamento-de-Veiculos <pedrohenriquemenezes76@gmail.com>
2026-01-20 21:44:45 -0300

    Dia 02: converte entrada do usuário para lista de números

--- src/main/java/br/com/DesafioSemana1/DesafioSemana1.java
+++ src/main/java/br/com/DesafioSemana1/DesafioSemana1.java
@@ -1,17 +1,29 @@
 package br.com.DesafioSemana1;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class DesafioSemana1 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite uma lista de números separados por espaço:");
         String entrada = scanner.nextLine();
 
-        System.out.println("Você digitou:");
-        System.out.println(entrada);
+        String[] valores = entrada.split(" ");
+
+        List<Double> numeros = new ArrayList<>();
+
+        for (String valor : valores) {
+            double numero = Double.parseDouble(valor);
+            numeros.add(numero);
+        }
+
+        System.out.println("Números convertidos:");
+        System.out.println(numeros);
 
         scanner.close();
     }
